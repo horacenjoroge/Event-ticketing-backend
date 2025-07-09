9d305b55825fe99d8c7629e6eb686f6dbeee681a
“horacenjoroge” <125595726+horacenjorge@users.noreply.github.com>
2025-07-10 00:34:53 +0300

    fix  :add create microservice method

--- event-ticketing-platform/apps/order-service/src/main.ts
+++ event-ticketing-platform/apps/order-service/src/main.ts
@@ -1,8 +1,26 @@
+// apps/order-service/src/main.ts
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { OrderServiceModule } from './order-service.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(OrderServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    OrderServiceModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [
+          process.env.RABBITMQ_URL || 'amqp://admin:admin123@rabbitmq:5672',
+        ],
+        queue: 'order_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+
+  await app.listen();
+  console.log('Order Service is listening on RabbitMQ order_queue');
 }
 bootstrap();
